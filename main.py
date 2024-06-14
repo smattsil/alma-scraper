762aedbb2e2c23b8dadc19aa312e262b6ecf3cb0
steven <steven.matt.sil@gmail.com>
2024-06-14 12:36:52 +0700

    testing out headers

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from dataclasses import dataclass
+from typing import Annotated
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
 
+@dataclass
+class Credential:
+    school: str
+    username: str
+    password: str
 
 @app.get("/")
 def read_root():
     return {"key": "Hello world!"}
+
+@app.get("/authenticity")
+def read_items(school: Annotated[str, Header()], username: Annotated[str, Header()], password: Annotated[str, Header()]):
+    return {"message": f'Successfully logged in as {username}!'}
